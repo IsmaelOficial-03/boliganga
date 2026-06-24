ccc3a7c693c04cb90345fef6f3396752c14186e5
IsmaelOficial-03 <ismaelleonh66@gmail.com>
2026-06-24 16:14:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log("!Hola Bienvenido¡")
-let currentStatus = "Viendo Netflix";
-currentStatus = "Relajándome en la playa";
-console.log(currentStatus);
+const flipCard = document.getElementById('flipCard');
+    const registerText = document.getElementById('registerText');
+    const loginText = document.getElementById('loginText');
+
+    registerText.addEventListener('click', () => {
+        flipCard.style.transform = 'rotateY(180deg)';
+    });
+
+    loginText.addEventListener('click', () => {
+        flipCard.style.transform = 'rotateY(0deg)';
+    });
